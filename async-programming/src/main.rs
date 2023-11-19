b54d59750c56de2a87cc1c12ea2281406a9874d5
XuGuangnian <gengen.wang@outlook.com>
2023-11-19 22:35:04 +0800

    Pin struct and Unpin/!Unpin trait(async-book)

--- async-programming/src/main.rs
+++ async-programming/src/main.rs
@@ -3,11 +3,13 @@
 mod async_await;
 mod async_intro;
 mod futures;
+mod phantom_pinned;
 mod pin_unpin;
 
 fn main() {
     async_intro::run();
     futures::run();
     pin_unpin::run();
+    phantom_pinned::run();
     async_await::run();
 }
